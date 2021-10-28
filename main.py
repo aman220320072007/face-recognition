66d5cbda1c068ce8c2d7108eef40ad208a203839
aman220320072007 <75264859+aman220320072007@users.noreply.github.com>
2021-10-28 11:36:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import sys
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("/home/amandeepbeuria/PycharmProjects/pythonProject1/known/aman.jpg")
+obama_image = face_recognition.load_image_file("IMAGE")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("/home/amandeepbeuria/PycharmProjects/pythonProject1/known/barack_obama.jpg")
+biden_image = face_recognition.load_image_file("IMAGE")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
